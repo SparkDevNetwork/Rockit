804f5a784ecdf186b3e2ce41b626bcdedd866f52
nairdo <nairdo@gmail.com>
2014-06-10 15:49:52 -0700

    + tutorial 1: Hello World

---
+++ RockWeb/Plugins/org_RockSolidChurch/HelloWorld.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="HelloWorld.ascx.cs" Inherits="Plugins_org_RockSolidChurch_HelloWorld" %>
+
+<h1>Hello World</h1>
---
+++ RockWeb/Plugins/org_RockSolidChurch/HelloWorld.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Plugins_org_RockSolidChurch_HelloWorld : Rock.Web.UI.RockBlock
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
